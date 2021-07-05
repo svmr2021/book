6e4a53eddbbdd0aa9ab7e09e4480311c60d0c714
svmr2021 <saidamir2020t@gmail.com>
2021-07-05 15:33:56 +0500

    url changed

--- LibraryProject/urls.py
+++ LibraryProject/urls.py
@@ -31,6 +31,6 @@ urlpatterns = [
     path('api/auth/', include('djoser.urls')),
     path('api/auth_token/', include('djoser.urls.authtoken')),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
